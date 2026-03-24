f13f14af5fa16db760bfae8e6aafaf1de3088bec
Vladyslav Horbachov <0965142838gvs@gmail.com>
2026-03-24 08:46:39 +0100

    feat: add EmojiAgent

--- src/Agents/Fun/IEmoji.cs
+++ src/Agents/Fun/IEmoji.cs
@@ -5,5 +5,5 @@ public interface IEmoji : IAgent
     static string IAgent.AgentDisplayName => "Emoji";
     static string IAgent.AgentDescription => "Translates text to emoji";
     static string[] IAgent.AgentCapabilities => ["emoji", "fun"];
-    static string IAgent.AgentInstructions => "You translate text into creative emoji representations. Be expressive.";
+    static string IAgent.AgentInstructions => "You translate text into creative emoji representations. Be expressive and fun.";
 }
