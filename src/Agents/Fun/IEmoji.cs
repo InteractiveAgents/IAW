d74b935ef7dc81aec777adc5a691e8fc18f2d355
Vladyslav Horbachov <0965142838gvs@gmail.com>
2026-03-24 08:09:04 +0100

    fix: add EmojiAgent

---
+++ src/Agents/Fun/IEmoji.cs
@@ -0,0 +1,9 @@
+using Core.Contracts;
+namespace IAW.Agents.Fun;
+public interface IEmoji : IAgent
+{
+    static string IAgent.AgentDisplayName => "Emoji";
+    static string IAgent.AgentDescription => "Translates text to emoji";
+    static string[] IAgent.AgentCapabilities => ["emoji", "fun"];
+    static string IAgent.AgentInstructions => "You translate text into creative emoji representations. Be expressive.";
+}
