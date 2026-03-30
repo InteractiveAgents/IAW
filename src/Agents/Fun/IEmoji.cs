efff7d9c417d16466fa29f3b069a43bdc2a7831d
Vladyslav Horbachov <0965142838gvs@gmail.com>
2026-03-30 14:35:11 +0200

    Remove fun folder with test agents with self-improving loop
    Co-Authored-By: Eugene <59283295+ScientistFromMars@users.noreply.github.com>

--- src/Agents/Fun/IEmoji.cs
+++
@@ -1,9 +0,0 @@
-using Core.Contracts;
-namespace IAW.Agents.Fun;
-public interface IEmoji : IAgent
-{
-    static string IAgent.AgentDisplayName => "Emoji";
-    static string IAgent.AgentDescription => "Translates text to emoji";
-    static string[] IAgent.AgentCapabilities => ["emoji", "fun"];
-    static string IAgent.AgentInstructions => "You translate text into creative emoji representations. Be expressive and fun.";
-}
